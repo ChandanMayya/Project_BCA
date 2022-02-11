b3b86fbbabc0e055957a82bfe6e7c93354ba50dc
Soorya_Kumar_M_H <79861274+Mahadhyaya@users.noreply.github.com>
2022-02-11 08:21:01 +0530

    Bugs fixed
    Problem in increments and closing brackets '('  and when it reached the end it should pop out all elements 
    These problems are fixed.

--- programs/postfix.c
+++ programs/postfix.c
@@ -1,59 +1,69 @@
 #include<stdio.h>
+#include<string.h>
 
-char postfix[50],infix[50],stack[50];
-int top=-1;
-
-void inftopost();
 void push(char);
+int pre(char);
+void evaluation();
 char pop();
-int preceed(char);
 
-void inftopost(){
-	char symbol;
-	int i=0,k=0;
-	while(symbol=infix[i++]!='\0'){
-		if(symbol=='(')
-			push('(');
-		else if(symbol==')')
-			while(stack[top]!='(')
-				postfix[k++]=pop();
-		else if((symbol=='+')||(symbol=='-')||(symbol=='/')||(symbol=='*')){
-			while(preceed(stack[top])>=preceed(symbol))
-				postfix[k++]=pop();
-			push(symbol);
-		}else{
-			postfix[k++]=symbol;	
-		}				
-	}	
-}
+int top = 0;
+char prefix[50], stack[50], postfix[50];
 
-void push(char symbol){
-	stack[top++]=symbol;
-}
-
-char pop(){
-	char ele;
-	ele=stack[top--];
-	return ele;
-}
-
-int preceed(char symbol){
-	switch(symbol){
-		case '(': return 0;
-		case '+':
-		case '-': return 1;
-		case '/':
-		case '*': return 2;
-	}	
-}
-
-void main(){
-	printf("Enter the infix expression:\t");
-	gets(infix);
-	push("$");
-	inftopost();
-	printf("The postfix expression is:\t");
+void main() {
+	printf("Enter the infix exp:");
+	scanf("%s", prefix);
+	evaluation();
+	printf("The postfix exp:");
 	puts(postfix);
 }
-	
 
+void push(char ch) {
+	stack[++top] = ch;
+}
+
+char pop() {
+	return stack[top--];
+}
+
+int pre(char c) {
+	switch(c) {
+		case '(' : 
+		case ')' : return 0;
+		case '+' :
+		case '-' : return 1;
+		case '*' :
+		case '/' : return 2;
+		case '^' : return 3;
+	}
+}
+
+void evaluation() {
+	int i = 0, r = 0;
+	char symbol;
+	while ((symbol = prefix[i++]) != '\0') {
+		if (symbol == '(') {
+			push('(');
+		} else if (symbol == ')') {
+			while (stack[top] != '(') {
+				postfix[r++] = pop();
+			}	
+			if (stack[top] == '(') {
+				pop();
+			}
+		} else if (symbol == '+' || symbol == '-' || symbol == '/' || symbol == '*' || symbol == '^') {
+			while (pre(stack[top]) >= pre(symbol)) {
+				postfix[r++] = pop();
+			}
+			push(symbol);
+		} else {
+			postfix[r++] = symbol;
+		}
+	}
+	while (top != 0) {
+		if (stack[top] == '(' || stack[top] == ')') {
+			top--;
+		} else {
+			postfix[r++] = pop();
+		}
+	}
+}
